5076dd98a14e3ff57d1efaf5d3c52d17446ceec0
Simon <fctinstean@gmail.com>
2022-06-02 11:46:40 +0200

    Flower migration

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('comments')->insert(
+            [
+                'comment' => 'Such a beautiful tulip',
+                'flower_id' => 1
+            ]
+        );
+
+        DB::table('comments')->insert(
+            [
+                'comment' => 'Jane is dying, help us.',
+                'flower_id' => 3
+            ]
+        );
+    }
+}
---
+++ database/seeders/FlowerSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FlowerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('flowers')->insert(
+            [
+                'name' => 'Tulip',
+                'price' => 5
+            ]
+        );
+
+        DB::table('flowers')->insert(
+            [
+                'name' => 'Orchid',
+                'price' => 10
+            ]
+        );
+
+        DB::table('flowers')->insert(
+            [
+                'name' => 'Lily',
+                'price' => 2
+            ]
+        );
+    }
+}
